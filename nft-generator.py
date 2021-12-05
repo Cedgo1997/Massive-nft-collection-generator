4fb093a3f74fe73360044ff1e4f058e27a3806b2
cedgo <cedgo1997@gmail.com>
2021-12-05 12:29:42 -0400

    Add: Recurrence solution. Select choices function now works as a recurrence. get images collection get all images in fixed order.

--- nft-generator.py
+++ nft-generator.py
@@ -4,14 +4,42 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import cv2
 import glob
+import inquirer
+
+# Get folder list name:
+def get_folder_list_names():
+    return os.listdir('./images')
+
+def get_images_collection(path_group):
+    images_group = []
+    for path in path_group:
+        images_group.append(get_images(path))
+    return images_group
+
+# Get NFT image order of components function:
+def select_choices(options, order):
+    choices = options
+    # Trigger inquirer.
+    questions = [
+    inquirer.List('size',
+                    message="What's the first layer group?",
+                    choices= choices,
+                ),
+    ]
+    # Saving sigle answer.
+    answer = inquirer.prompt(questions)['size']
+    order.append(answer)
+    choices.remove(answer) # Removing answer to tigger function again.
+    if len(choices) > 0:
+        select_choices(choices, order)
+    else:
+        print(order)
+
+options = get_folder_list_names()
+select_choices(options, [])
+
 
 # Get Images from folder:
-
-def get_folder_list_names():
-    print(os.listdir('./images'))
-
-get_folder_list_names()
-
 def get_images(folder_name):
     image_list = []
 
@@ -23,7 +51,7 @@ def get_images(folder_name):
 
 image_list = get_images('background')
 
-print(image_list[0].filename)
+#print(image_list[0].filename)
 
 raw_input = input
 
