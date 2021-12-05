76d0aaacf52c99828ce14eedf00c8042e76b241c
cedgo <cedgo1997@gmail.com>
2021-12-05 13:07:34 -0400

    Re organize functions return images on recurrence

--- nft-generator.py
+++ nft-generator.py
@@ -6,15 +6,32 @@ import cv2
 import glob
 import inquirer
 
+images = []
+
 # Get folder list name:
+
+
 def get_folder_list_names():
     return os.listdir('./images')
 
+
+# Get Images from folder:
+def get_images(folder_name):
+    image_list = []
+    for filename in glob.glob('./images/{}/*.png'.format(folder_name)):  # assuming gif
+        im = Image.open(filename)
+        image_list.append(im)
+
+    return image_list  # Array with all images in a folder
+
+
+# Get ALL images in nested arrays:
 def get_images_collection(path_group):
     images_group = []
     for path in path_group:
         images_group.append(get_images(path))
     return images_group
+
 
 # Get NFT image order of components function:
 def select_choices(options, order):
@@ -22,35 +39,27 @@ def select_choices(options, order):
     choices = options
     # Trigger inquirer.
     questions = [
-    inquirer.List('size',
-                    message="What's the first layer group?",
-                    choices= choices,
-                ),
+        inquirer.List('size',
+                      message="What's the first layer group?",
+                      choices=choices,
+                      ),
     ]
     # Saving sigle answer.
     answer = inquirer.prompt(questions)['size']
     order.append(answer)
-    choices.remove(answer) # Removing answer to tigger function again.
+    choices.remove(answer)  # Removing answer to tigger function again.
     if len(choices) > 0:
-        select_choices(choices, order)
+        return select_choices(choices, order)
     else:
         images = get_images_collection(order)
         return images
 
 
-# Get Images from folder:
-def get_images(folder_name):
-    image_list = []
+folder_list = get_folder_list_names()
 
-    for filename in glob.glob('./images/{}/*.png'.format(folder_name)): #assuming gif
-        im=Image.open(filename)
-        image_list.append(im)
+images = select_choices(folder_list, [])
 
-    return image_list # Array with all images in a folder
-
-image_list = get_images('background')
-
-#print(image_list[0].filename)
+print(images)
 
 raw_input = input
 
